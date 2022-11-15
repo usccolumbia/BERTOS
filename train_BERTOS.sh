2e9deb2a9187fe88ec966b841871c4e84a35daf1
MLEG <jianjunh@cse.sc.edu>
2022-11-15 01:44:02 -0500

    Update train_BERTOS.sh

--- train_BERTOS.sh
+++ train_BERTOS.sh
@@ -1,8 +1,8 @@
 python train_BERTOS.py  \
        --config_name ./random_config/  \
-       --dataset_name materials_icsd.py   \
+       --dataset_name materials_icsd_cn.py   \
        --max_length 100  \
        --per_device_train_batch_size 256 \
        --learning_rate 1e-3  \
        --num_train_epochs 500    \
-       --output_dir ./output_icsd
+       --output_dir ./output_icsdcn
