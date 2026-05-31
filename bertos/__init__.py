b845c3d93d22aa68c23e4a3e4fcb5f569dde6657
MLEG <jianjunh@cse.sc.edu>
2026-05-31 02:59:59 -0400

    Create __init__.py

---
+++ bertos/__init__.py
@@ -0,0 +1,7 @@
+git mv getOS.py bertos/getOS.py
+git mv checkCN.py bertos/checkCN.py
+git mv train_BERTOS.py bertos/train_BERTOS.py
+git mv materials_icsd.py bertos/materials_icsd.py
+git mv materials_icsd_cn.py bertos/materials_icsd_cn.py
+git mv materials_icsd_cno.py bertos/materials_icsd_cno.py
+git mv materials_icsd_o.py bertos/materials_icsd_o.py
